4a223106d855b86dec6b7b5406aa750611ff07b3
Anand Upadhyay <anandu@gumromedia.com>
2016-02-01 11:08:06 -0500

    solution 6

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,3 +1,15 @@
 module StringCalculator
+  def self.add(string)
+    return string = 0 if string.empty?
 
+    if string.include?("//")
+      delimiter = string[2]
+      string = string[3..-1]
+      puts "The string is now #{string}"
+      puts delimiter
+    end
+    puts values = string.split(/[\n#{delimiter},]/)
+    values.map(&:to_i).inject(&:+)
+  end
+  puts "The output is #{StringCalculator.add("//;\n2;5")}"
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe StringCalculator, "#add" do
     expect(StringCalculator.add("")).to eql(0)
   end
 
+  it "returns the integer of a single string number" do
+    expect(StringCalculator.add("0")).to eql(0)
+    expect(StringCalculator.add("5")).to eql(5)
+  end
+
+  it "returns the sum of all numbers in a comma delimited string" do
+    expect(StringCalculator.add("1,2")).to eq(3)
+    expect(StringCalculator.add("7,9")).to eq(16)
+    expect(StringCalculator.add("12,45")).to eq(57)
+  end
+
+  it "parses the new line character" do
+    expect(StringCalculator.add("1\n2,3")).to eq(6)
+  end
+
+  it "should use the delimiter set by input" do
+    # “//;\n2;5”
+    expect(StringCalculator.add("//;\n2;5")).to eq(7)
+  end
+
 end
